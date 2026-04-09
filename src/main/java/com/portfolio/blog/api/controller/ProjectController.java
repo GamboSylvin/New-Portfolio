48c6191984d2efe4871f82ac722ff31d32670b85
Gambo <sylviangams@gmail.com>
2026-04-09 19:44:20 +0800

    added cors

--- src/main/java/com/portfolio/blog/api/controller/ProjectController.java
+++ src/main/java/com/portfolio/blog/api/controller/ProjectController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://new-portfolio-front-eta.vercel.app/")
 @Validated
 @RestController
 public class ProjectController {
